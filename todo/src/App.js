a0a5d1b0dd8f39718d3e031b0cb0ffe894c4eb4a
Alex Reber <rebera@alfredstate.edu>
2021-02-03 16:20:32 -0500

    started working on the input element and its respective commit

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,6 +1,6 @@
 // import logo from './logo.svg';
 import {useState} from 'react';
-
+import React from 'react';
 import logo from './tek_logo.png';
 import './App.css';
 import Todo from './Components/Todo';
@@ -8,6 +8,26 @@ import Todo from './Components/Todo';
 function App() {
 
   const [todos, setTodos] = useState(["eat","sleep","code","pray","repeat"]);  //sample todo items.  these will need to be changed in your app.  Just filler todos, although they are quite important!
+  const [addToList, setAddToList] = React.useState({ newToDo : ""});
+
+  const handleChange = (e) => {
+    
+    console.log(e.target.name);
+    setAddToList({
+      ...addToList,
+      [e.target.name] : e.target.value
+    })
+    console.log(addToList);
+  }
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //let selectUl = document.querySelector('ul'),
+    //let listItem = document.createElement('li'),
+    //let selectUl.appendChild(listItem.value),
+    console.log(addToList);
+  }
 
   return (
     <div className="App">
@@ -20,6 +40,13 @@ function App() {
           <Todo item="2"/>
           <Todo item="3"/>
           {todos.map( (t,i) => <Todo key={i} item={t}/>)}
+          <ul>
+            hello world
+          </ul>
+          <form onSubmit={handleSubmit}>
+            <input name="newToDo" onChange={handleChange} placeholder="Enter a task"/>
+            <button type="submit">Add</button>
+          </form>
         </div>
       </header>
     </div>
